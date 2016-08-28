ea8642079e0b90c36cafe49028c7f65c417df042
Nandhakumar Chellamuthu <nandhakumar.tc@gmail.com>
2016-08-28 15:41:39 +0530

    JAXB

---
+++ src/com/xml/jaxb/model/Bookstore.java
@@ -0,0 +1,46 @@
+package com.xml.jaxb.model;
+
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by nandh on 28-08-2016.
+ */
+//This statement means that class "Bookstore.java" is the root-element of our example
+@XmlRootElement(namespace = "com.xml.jaxb.model")
+public class Bookstore {
+    // XmLElementWrapper generates a wrapper element around XML representation
+    @XmlElementWrapper(name = "bookList")
+    // XmlElement sets the name of the entities
+    @XmlElement(name = "book")
+    private ArrayList<Book> bookList;
+    private String name;
+    private String location;
+
+    public ArrayList<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(ArrayList<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
