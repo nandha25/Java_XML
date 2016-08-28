f2caa0c1c6a1b4e86c80ae6c1249c873e8306241
Nandhakumar Chellamuthu <nandhakumar.tc@gmail.com>
2016-08-28 13:48:11 +0530

    created Model

--- src/com/xml/reader/Main.java
+++ src/com/xml/reader/Main.java
@@ -1,3 +1,5 @@
+package com.xml.reader;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/com/xml/reader/StaXParser.java
@@ -0,0 +1,7 @@
+package com.xml.reader;
+
+/**
+ * Created by nandh on 28-08-2016.
+ */
+public class StaXParser {
+}
